5cb7b5a8d1aa35f7dc4179ce07c6c1ab804db8ed
Vanisus <ieremin755@gmail.com>
2023-04-12 18:06:39 +0400

    Add "shedule timetable"

--- src/main/java/ru/vanisus/App.java
+++ src/main/java/ru/vanisus/App.java
@@ -1,36 +1,39 @@
 package ru.vanisus;
 import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.UpdatesListener;
-import com.pengrad.telegrambot.model.BotCommand;
-import com.pengrad.telegrambot.model.Update;
+import com.pengrad.telegrambot.model.MessageEntity;
+import com.pengrad.telegrambot.request.DeleteMessage;
 import com.pengrad.telegrambot.request.SendMessage;
-import com.pengrad.telegrambot.request.SetMyCommands;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
-
-
 
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URISyntaxException;
 import java.net.URL;
-import java.time.DayOfWeek;
-import java.time.LocalDate;
+import java.time.*;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 import static ru.vanisus.DAO.*;
 
 public class App {
  	public static void main(String[] args) {
-		String TOKEN = "6107692340:AAEUQDi7nTDQAnp7kqvVrhY60QWxjPIe15A";
+		String TOKEN = "6108997566:AAFx0BbXz8M2b2AAYkEZw2D1z3D85xdwAvM";
 		TelegramBot bot = new TelegramBot(TOKEN);
-//		TimeZone tz = TimeZone.getTimeZone("Europe/Samara");
-//		Calendar cal = Calendar.getInstance(tz);
-//		cal.set(Calendar.HOUR_OF_DAY, 9);
-//		cal.set(Calendar.MINUTE, 19);
-//		cal.set(Calendar.SECOND, 0);
-//		cal.set(Calendar.MILLISECOND, 0);
-//		long delay = cal.getTimeInMillis() - System.currentTimeMillis();
+
+		ZonedDateTime nowInSamara = ZonedDateTime.now(ZoneId.of("Europe/Samara"));
+		LocalDateTime targetTime = LocalDateTime.of(LocalDate.now(), LocalTime.of(12, 28));
+		ZonedDateTime targetTimeInSamara= ZonedDateTime.of(targetTime, ZoneId.of("Europe/Samara"));
+		DayOfWeek date = LocalDate.EPOCH.getDayOfWeek();
+
+		if (nowInSamara.isAfter(targetTimeInSamara)) {
+			targetTimeInSamara = targetTimeInSamara.plusDays(1);
+		}
+		long delayInMillis = Duration.between(nowInSamara, targetTimeInSamara).toMillis();
+
 		bot.setUpdatesListener(updates -> {
 			updates.forEach(System.out::println);
 				updates.forEach(update -> {
@@ -38,126 +41,138 @@ public class App {
 				Element day;
 				Element week;
 				String div;
-				bot.execute(new SetMyCommands(new BotCommand("today", "Расписание на сегодня"), new BotCommand("tommorow", "Расписание на завтра"), new BotCommand("thisWeek", "Расписание на текущую неделю"),new BotCommand("nextWeek", "Расписание на следующую неделю")));
-				if (!findId(update.message().from().id())) {
+
+				ScheduledExecutorService executor = Executors.newSingleThreadScheduledExecutor();
+				executor.schedule(() -> {
+					Document docShedule;
+					Element dayShedule;
+					String divShedule;
+					System.out.println(date.getValue());
+					if(isShedule(update.message().from().id())) {
+						if(isMessageExists(update.message().chat().id()))
+							bot.execute(new DeleteMessage(update.message().chat().id(), getMessageId(update.message().chat().id())));
+						try {
+								docShedule = Jsoup.connect(getURL(update.message().from().id())).get();
+							} catch (IOException e) {
+								throw new RuntimeException(e);
+							}
+						dayShedule = docShedule.select("div.day").get(getDayNumberNew(LocalDate.now()) + 1);
+						divShedule = getDayTimetable(dayShedule);
+						bot.execute(new SendMessage(update.message().chat().id(), "Расписание на завтра: " + LocalDate.now().plusDays(1) + "\n\n" + divShedule));
+						if(isMessageExists(update.message().chat().id()))
+							updateMessage(update.message().chat().id(), (long) (update.message().messageId() + 1));
+						else
+							addMessage(update.message().chat().id(), (long) (update.message().messageId() + 1));
+						}
+					executor.shutdown();
+				}, delayInMillis, TimeUnit.MILLISECONDS);
+
+					if (!findId(update.message().from().id())) {
 						if(update.message().text().equalsIgnoreCase("/start"))
 							bot.execute(new SendMessage(update.message().chat().id(), "Вам необходимо прислать ссылку на расписание вашей группы"));
 						String url = update.message().text();
 						System.out.println(url);
 						if(!url.equalsIgnoreCase("/start")) {
-							if(urlChecker(update, url)) {
+							if(urlChecker(url)) {
 								bot.execute(new SendMessage(update.message().chat().id(), "Принял, отлично, теперь вы можете просматривать свое расписание благодаря мне :)"));
 								//User user = new User(update.message().from().id(), update.message().from().firstName(), url);
-								addRow(update.message().from().id(), url);
+								addUser(update.message().from().id(), url);
 							} else
 								bot.execute(new SendMessage(update.message().chat().id(), "Вы прислали мне какую-то непонятную ссылку, пришлите ссылку формата \"https://rasp.sstu.ru/rasp/group/\""));
 						}
 				}
-				if(update.message().text().equalsIgnoreCase("расписание на сегодня") || update.message().text().equalsIgnoreCase("/command1"))
-				{
-					//bot.execute(new SendMessage(update.message().chat().id() ,"ЛАЛАЛА Я СЛОМАЛСЯ"));
-					try {
-						doc = Jsoup.connect(getURL(update.message().from().id())).get();
-					} catch (IOException e) {
-						bot.execute(new SendMessage(update.message().chat().id(), "У вас не добавлена ссылка!"));
-					}
+				if(findId(update.message().from().id())) {
+					if (update.message().text().equalsIgnoreCase("/today")) {
+//					bot.execute(new SendMessage(update.message().chat().id() ,"ЛАЛАЛА Я СЛОМАЛСЯ"));
+						try {
+							doc = Jsoup.connect(getURL(update.message().from().id())).get();
+						} catch (IOException e) {
+							bot.execute(new SendMessage(update.message().chat().id(), "У вас не добавлена ссылка!"));
+						}
+						assert doc != null;
 						day = doc.select("div.day").get(getDayNumberNew(LocalDate.now()));
 						div = getDayTimetable(day);
 						bot.execute(new SendMessage(update.message().chat().id(), div));
 
 					}
-				if (update.message().text().equalsIgnoreCase("расписание на завтра") || update.message().text().equalsIgnoreCase("/command2")) {
-					try {
-						doc = Jsoup.connect(getURL(update.message().from().id())).get();
-					} catch (IOException e) {
-						bot.execute(new SendMessage(update.message().chat().id(), "У вас не добавлена ссылка!"));
+					if (update.message().text().equalsIgnoreCase("/tomorrow")) {
+						try {
+							doc = Jsoup.connect(getURL(update.message().from().id())).get();
+						} catch (IOException e) {
+							bot.execute(new SendMessage(update.message().chat().id(), "У вас не добавлена ссылка!"));
+						}
+						assert doc != null;
+						day = doc.select("div.day").get(getDayNumberNew(LocalDate.now()) + 1);
+						div = getDayTimetable(day);
+						bot.execute(new SendMessage(update.message().chat().id(), div));
 					}
-					day = doc.select("div.day").get(getDayNumberNew(LocalDate.now()) + 1);
-					div = getDayTimetable(day);
-					bot.execute(new SendMessage(update.message().chat().id(), div));
-				}
-				if(update.message().text().equalsIgnoreCase("расписание на текущую неделю") || update.message().text().equalsIgnoreCase("/command3"))
-				{
+					if (update.message().text().equalsIgnoreCase("/currentweek")) {
 //					bot.execute(new SendMessage(update.message().chat().id(), "ЫЫЫ"));
-					try {
-						doc = Jsoup.connect(getURL(update.message().from().id())).get();
-					} catch (IOException e) {
-						bot.execute(new SendMessage(update.message().chat().id(), "У вас не добавлена ссылка!"));
-					}
-					week = doc.select("div.week").get(0);
-					div = getWeekTimetable(week);
-					bot.execute(new SendMessage(update.message().chat().id(), div));
-				}
-
-				if(update.message().text().equalsIgnoreCase("расписание на следующую неделю") || update.message().text().equalsIgnoreCase("/command4")) {
-					try {
-						doc = Jsoup.connect(getURL(update.message().from().id())).get();
-					} catch (IOException e) {
-						bot.execute(new SendMessage(update.message().chat().id(), "У вас не добавлена ссылка!"));
-					}
-					try {
-						week = doc.select("div.week").get(1);
+						try {
+							doc = Jsoup.connect(getURL(update.message().from().id())).get();
+						} catch (IOException e) {
+							bot.execute(new SendMessage(update.message().chat().id(), "У вас не добавлена ссылка!"));
+						}
+						assert doc != null;
+						week = doc.select("div.week").get(0);
 						div = getWeekTimetable(week);
 						bot.execute(new SendMessage(update.message().chat().id(), div));
-					} catch (IndexOutOfBoundsException e) {
-						bot.execute(new SendMessage(update.message().chat().id(), "Расписание на следующую неделю пока недоступно :)"));
 					}
 
-				}
-//				if(update.message().text().equalsIgnoreCase("обновить ссылку")) {
-//					bot.execute(new SendMessage(update.message().chat().id(), "Хорошо, пришлите мне новую ссылку"));
-//					String url = update.message().text();
-//					System.out.println(url);
-//					if(!url.equalsIgnoreCase("обновить ссылку"))
-//						if(urlChecker(update, url)) {
-//							updateURL(update.message().from().id(), url);
-//							bot.execute(new SendMessage(update.message().chat().id(), "Принял, отлично, ссылка обновлена :)"));}
-//						else
-//							bot.execute(new SendMessage(update.message().chat().id(), "Вы прислали мне какую-то непонятную ссылку, пришлите ссылку формата \"https://rasp.sstu.ru/rasp/group/\""));
-//					}
+					if (update.message().text().equalsIgnoreCase("/nextweek")) {
+						try {
+							doc = Jsoup.connect(getURL(update.message().from().id())).get();
+						} catch (IOException e) {
+							bot.execute(new SendMessage(update.message().chat().id(), "У вас не добавлена ссылка!"));
+						}
+						try {
+							assert doc != null;
+							week = doc.select("div.week").get(1);
+							div = getWeekTimetable(week);
+							bot.execute(new SendMessage(update.message().chat().id(), div));
+						} catch (IndexOutOfBoundsException e) {
+							bot.execute(new SendMessage(update.message().chat().id(), "Расписание на следующую неделю пока недоступно :)"));
+						}
 
-//					TimerTask task = new TimerTask() {
-//					@Override
-//					public void run() {
-//						Document doc = null;
-//						Element day;
-//						String div;
-//						try {
-//							doc = Jsoup.connect(getURL(update.message().from().id())).get();
-//						} catch (IOException e) {
-//							bot.execute(new SendMessage(update.message().chat().id(), "У вас не добавлена ссылка!"));
+					}
+
+					if (update.message().text().equalsIgnoreCase("/shedule")) {
+						Long temp = update.message().from().id();
+						updateShedule(temp, !isShedule(temp));
+						if (isShedule(temp))
+							bot.execute(new SendMessage(update.message().chat().id(), "Теперь вы будете получать сообщение с расписанием на завтрашний день"));
+						else
+							bot.execute(new SendMessage(update.message().chat().id(), "Теперь вы не будете получать сообщение с расписанием на завтрашний день"));
+
+					}
+
+					if (update.message().text().equalsIgnoreCase("/help")) {
+						bot.execute(new SendMessage(update.message().chat().id(), "Сам себе помоги"));
+					}
+
+
+					if (update.message().text().equalsIgnoreCase("/updatelink")) {
+						bot.execute(new DeleteMessage(update.message().chat().id(), update.message().messageId()));
+						bot.execute(new SendMessage(update.message().chat().id(), "Пока нет такой возможности :("));
+//						bot.execute(new SendMessage(update.message().chat().id(), "Хорошо, пришлите мне новую ссылку"));
+//						div = update.message().text();
+//						if (!update.message().text().equalsIgnoreCase("/updatelink")) {
+//							if (urlChecker(div)) {
+//								updateURL(update.message().from().id(), div);
+//								bot.execute(new SendMessage(update.message().chat().id(), "Принял, отлично, ссылка обновлена :)"));
+//							} else
+//								bot.execute(new SendMessage(update.message().chat().id(), "Вы прислали мне какую-то непонятную ссылку, пришлите ссылку формата \"https://rasp.sstu.ru/rasp/group/\""));
 //						}
-//						day = doc.select("div.day").get(getDayNumberNew(LocalDate.now()) + 1);
-//						div = getDayTimetable(day);
-//						bot.execute(new SendMessage(update.message().chat().id(), div));
-//				} };
-//				Timer timer = new Timer();
-//				timer.schedule(task,delay);
+					}
+				}
 
 
-			});
-			return UpdatesListener.CONFIRMED_UPDATES_ALL;
-		});
-	}
-
-//	public static ReplyKeyboardMarkup createKeyboard() {
-//		KeyboardButton today = new KeyboardButton("расписание на сегодня");
-//		KeyboardButton tomorrow = new KeyboardButton("расписание на завтра");
-//		KeyboardButton thisWeek = new KeyboardButton("расписание на текущую неделю");
-//		KeyboardButton nextWeek = new KeyboardButton("расписание на следующую неделю");
-//		ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup(today, tomorrow, thisWeek, nextWeek);
-//		return replyKeyboardMarkup;
-//
-//
-//	 }
-
-	public static Boolean urlChecker(Update update, String url) {
+	}); return UpdatesListener.CONFIRMED_UPDATES_ALL; });
+	 }
+	public static Boolean urlChecker( String url) {
 		String rightUrl = "https://rasp.sstu.ru/rasp/group/";
-		String testUrl = url.replaceAll("[0-9]", "");
-		if (!(testUrl.equals(rightUrl) && urlValidator(url) && url.length() != 0))
-			return false;
-		else
-			return  true;
+		String testUrl = url.replaceAll("\\d", "");
+		return testUrl.equals(rightUrl) && urlValidator(url) && url.length() != 0;
 	}
 	public static boolean urlValidator(String url)
 	{
@@ -173,12 +188,6 @@ public class App {
 	 public static int getDayNumberNew(LocalDate date) {
 		DayOfWeek day = date.getDayOfWeek();
 		return day.getValue();
-	}
-
-	public static String getDayOfWeek(LocalDate date)
-	{
-		DayOfWeek day = date.getDayOfWeek();
-		return day.toString();
 	}
 
 	public static String getDayTimetable(Element day)
@@ -207,7 +216,7 @@ public class App {
 			 Element dayE = week.select("div.day").get(count);
 			 String dateOfTheDay = dayE.select("div.day > div.day-header").get(0).text();
 			 if (dateOfTheDay.length() != 0) {
-				 weekTimetable.append("// " + dateOfTheDay + "// ");
+				 weekTimetable.append("// ").append(dateOfTheDay).append("// ");
 				 weekTimetable.append("\n\n");
 				 resultingDay.append(getDayTimetable(dayE));
 				 weekTimetable.append(resultingDay);
--- src/main/java/ru/vanisus/DAO.java
+++ src/main/java/ru/vanisus/DAO.java
@@ -49,7 +49,7 @@ public interface DAO {
             return checkUser;
         }
 
-        static void addRow(Long userId, String url) {
+        static void addUser(Long userId, String url) {
             try{
                 Class.forName("com.mysql.cj.jdbc.Driver").getDeclaredConstructor().newInstance();
                 try (Connection conn = connectDB()){
@@ -97,5 +97,111 @@ public interface DAO {
                 System.out.println(ex);
             }
         }
+
+        static boolean isShedule(Long userId) {
+            Boolean temp = false;
+            try{
+                Class.forName("com.mysql.cj.jdbc.Driver").getDeclaredConstructor().newInstance();
+                try (Connection conn = connectDB()){
+                    java.sql.Statement statement = conn.createStatement();
+                    ResultSet resultSet = (statement.executeQuery("select shedule from timetable.User where UserId = " + userId));
+                    resultSet.next();
+                    temp = resultSet.getBoolean(1);
+                    System.out.println("Shedule was found");
+                }
+            }
+            catch(Exception ex){
+                System.out.println("Connection failed...");
+                System.out.println(ex);
+            }
+            return temp;
+        }
+
+        static void updateShedule(Long userId, Boolean bt) {
+            try{
+                Class.forName("com.mysql.cj.jdbc.Driver").getDeclaredConstructor().newInstance();
+                try (Connection conn = connectDB()){
+                    java.sql.Statement statement = conn.createStatement();
+                    statement.executeUpdate("update timetable.User set shedule = " + bt + " where UserId = " + userId);
+                    System.out.println("Shedule was updated");
+                }
+            }
+            catch(Exception ex){
+                System.out.println("Connection failed...");
+                System.out.println(ex);
+            }
+        }
+
+    static void addMessage(Long chatId, Long messageId) {
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver").getDeclaredConstructor().newInstance();
+            try (Connection conn = connectDB()){
+                java.sql.Statement statement = conn.createStatement();
+                int rows = statement.executeUpdate("INSERT INTO timetable.message(chatID, messageId) values (" + chatId + ", '" + messageId + "')");
+                System.out.println("Added: " + rows);
+            }
+        }
+        catch(Exception ex){
+            System.out.println("Connection failed...");
+            System.out.println(ex);
+        }
     }
+
+    static void updateMessage(Long chatId, Long messageId) {
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver").getDeclaredConstructor().newInstance();
+            try (Connection conn = connectDB()){
+                java.sql.Statement statement = conn.createStatement();
+                int rows = statement.executeUpdate("update timetable.message set messageid = " + messageId + " where chatId = " + chatId);
+                System.out.println("Added: " + rows);
+            }
+        }
+        catch(Exception ex){
+            System.out.println("Connection failed...");
+            System.out.println(ex);
+        }
+    }
+
+    static int getMessageId(Long chatId) {
+            int temp = 0;
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver").getDeclaredConstructor().newInstance();
+            try (Connection conn = connectDB()){
+                java.sql.Statement statement = conn.createStatement();
+                ResultSet resultSet = (statement.executeQuery("select messageId from timetable.Message where chatId = " + chatId));
+                resultSet.next();
+                temp = resultSet.getInt(1);
+                System.out.println("URL was found");
+            }
+        }
+        catch(Exception ex){
+            System.out.println("Connection failed...");
+            System.out.println(ex);
+        }
+        return temp;
+    }
+    static Boolean isMessageExists(Long chatId) {
+        boolean checkMessage = false;
+        int row = 0;
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver").getDeclaredConstructor().newInstance();
+            try (Connection conn = connectDB()){
+                java.sql.Statement statement = conn.createStatement();
+                ResultSet resultSet = statement.executeQuery("select * from timetable.Message where chatId = " + chatId);
+                while (resultSet.next()) {
+                    row++;
+                }
+                if(row >  0) {
+                    System.out.println("Message was found");
+                    checkMessage = true;
+                }
+            }
+        }
+        catch(Exception ex){
+            System.out.println("Connection failed...");
+            System.out.println(ex);
+        }
+        return checkMessage;
+    }
+}
 
